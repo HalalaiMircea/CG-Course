930b044521fba1cd83e64b2c4d09c19f9900041d
Halalai Mircea <boss.mircea67@yahoo.com>
2020-05-11 21:10:46 +0300

    Made textures example double buffered

--- TemeLab/tema9_2.cpp
+++ TemeLab/tema9_2.cpp
@@ -1,48 +1,31 @@
 #include <GL/freeglut.h>
 #include <SOIL.h>
 
-static int leftFirst = GL_TRUE;
+unsigned char *image1, *image2;
+int textureWidth1, textureHeight1, textureWidth2, textureHeight2;
 
-void LoadTexture() {
-    GLuint texture;
+void loadTexture() {
+    GLuint texture = 0;
     glGenTextures(1, &texture);
     glBindTexture(GL_TEXTURE_2D, texture);
 
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP);    // Set texture wrapping to GL_REPEAT
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
 
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-    int width, height;
-    unsigned char *image = SOIL_load_image("text_smiley_face.png", &width, &height, 0, SOIL_LOAD_RGB);
-    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, image);
-    // SOIL_free_image_data(image);
-    // glBindTexture(GL_TEXTURE_2D, 0);
 
-}
-
-void LoadTexture2() {
-    GLuint texture;
-    glGenTextures(1, &texture);
-    glBindTexture(GL_TEXTURE_2D, texture);
-
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP);    // Set texture wrapping to GL_REPEAT
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
-
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-    int width, height;
-    unsigned char *image = SOIL_load_image("text_earth.bmp", &width, &height, 0, SOIL_LOAD_RGB);
-    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, image);
-    // SOIL_free_image_data(image);
-    // glBindTexture(GL_TEXTURE_2D, 0);
+    image1 = SOIL_load_image("text_smiley_face.png", &textureWidth1, &textureHeight1, 0, SOIL_LOAD_RGB);
+    image2 = SOIL_load_image("text_earth.bmp", &textureWidth2, &textureHeight2, 0, SOIL_LOAD_RGB);
 }
 
 void init() {
     glEnable(GL_BLEND);
+    glEnable(GL_TEXTURE_2D);
     glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+    loadTexture();
+    glEnable(GL_CULL_FACE);
 }
-
 
 static void drawLeftTriangle() {
     /* draw yellow triangle on LHS of screen */
@@ -69,46 +52,42 @@ static void drawRightTriangle() {
 void display() {
     glClearColor(0, 0, 0, 1);
     glClear(GL_COLOR_BUFFER_BIT);
-    glColor3f(0.4, 0.3, 0.1);
 
     glPushMatrix();
-    glTranslatef(0, 2, 0);
-    if (leftFirst) {
+    {
+        glColor3f(0.4, 0.3, 0.1);
+        glTranslatef(0, 2, 0);
         drawLeftTriangle();
         drawRightTriangle();
     }
-    else {
-        drawRightTriangle();
-        drawLeftTriangle();
+    glPopMatrix();
+
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, textureWidth1, textureHeight1, 0, GL_RGB, GL_UNSIGNED_BYTE, image1);
+
+    glPushMatrix();
+    {
+        glScalef(2, 2, 1);
+        glBegin(GL_QUADS);
+        glTexCoord2f(0, 1); glColor4f(1.0, 0.1, 0.1, 0.9f); glVertex2f(-2, -1);
+        glTexCoord2f(1, 1); glColor4f(0.1, 1.0, 0.1, 0.9f); glVertex2f(0, -1);
+        glTexCoord2f(1, 0); glColor4f(1.0, 1.0, 0.1, 0.9f); glVertex2f(0, 1);
+        glTexCoord2f(0, 0); glColor4f(0.0, 0.1, 1.0, 0.9f); glVertex2f(-2, 1);
+        glEnd();
+
+        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, textureWidth2, textureHeight2, 0, GL_RGB, GL_UNSIGNED_BYTE, image2);
+
+        glColor4f(1.0, 1.0, 1.0, 0.7f);
+        glBegin(GL_QUADS);
+        glTexCoord2f(0, 1); glVertex2f(-2, -1);
+        glTexCoord2f(1, 1); glVertex2f(0, -1);
+        glTexCoord2f(1, 0); glVertex2f(0, 1);
+        glTexCoord2f(0, 0); glVertex2f(-2, 1);
+        glEnd();
     }
     glPopMatrix();
 
-    glEnable(GL_TEXTURE_2D);
-    LoadTexture();
-
-    glPushMatrix();
-    glScalef(2, 2, 1);
-    glBegin(GL_QUADS);
-    glTexCoord2f(1.0, 1.0); glColor4f(1.0, 0.1, 0.1,0.9f); glVertex2f(-2.0, -1.0);
-    glTexCoord2f(1.0, 0.0); glColor4f(0.1, 1.0, 0.1,0.9f); glVertex2f(-2.0, 1.0);
-    glTexCoord2f(0.0, 0.0); glColor4f(1.0, 1.0, 0.1,0.9f); glVertex2f(0.0, 1.0);
-    glTexCoord2f(0.0, 1.0); glColor4f(0.0, 0.1, 1.0,0.9f); glVertex2f(0.0, -1.0);
-    glEnd();
-
-    LoadTexture2();
-    glColor4f(1.0, 1.0, 1.0, 0.7f);
-    glPushMatrix();
-    glTranslatef(-4, 0, 0);
-    glBegin(GL_QUADS);
-    glTexCoord2f(0.0, 0.0); glVertex2f(2.0, -1.0);
-    glTexCoord2f(0.0, 1.0); glVertex2f(2.0, 1.0);
-    glTexCoord2f(1.0, 1.0); glVertex2f(4.0, 1.0);
-    glTexCoord2f(1.0, 0.0); glVertex2f(4.0, -1.0);
-    glEnd();
-    glPopMatrix();
-    glPopMatrix();
-
-    glFlush();
+    glutSwapBuffers();
+    glutPostRedisplay();
 }
 
 void reshape(int w, int h) {
@@ -127,10 +106,11 @@ void reshape(int w, int h) {
 }
 
 void keyboard(unsigned char key, int x, int y) {
-    switch (key) {
-        case 27:
-            exit(0);
-            break;
+    if (key == 27) {
+        SOIL_free_image_data(image1);
+        SOIL_free_image_data(image2);
+        glBindTexture(GL_TEXTURE_2D, 0);
+        exit(0);
     }
 }
 
@@ -138,10 +118,10 @@ int main(int argc, char **argv) {
     int width = 800, height = 600;
     glutInit(&argc, argv);
     glutSetOption(GLUT_MULTISAMPLE, 8);
-    glutInitDisplayMode(GLUT_SINGLE | GLUT_RGBA | GLUT_DEPTH | GLUT_MULTISAMPLE);
-    glutInitWindowPosition((glutGet(GLUT_SCREEN_WIDTH) - width), (glutGet(GLUT_SCREEN_HEIGHT) - height));
+    glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGBA | GLUT_DEPTH | GLUT_MULTISAMPLE);
+    glutInitWindowPosition((glutGet(GLUT_SCREEN_WIDTH) - width)/2, (glutGet(GLUT_SCREEN_HEIGHT) - height)/2);
     glutInitWindowSize(width, height);
-    glutCreateWindow(argv[0]);
+    glutCreateWindow("Exemplu Texturi");
     init();
     glutDisplayFunc(display);
     glutReshapeFunc(reshape);
